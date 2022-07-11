e52d09e1ec9b52b6dfee0e8653ec5575f5abdd26
0x-Lucifer <subham@0xytocin.com>
2022-07-11 14:35:41 +0530

    health check route added

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const passport = require("passport");
 const authRoute = require("./authRoute");
 const usersRoute = require("./usersRoute");
 router.use(authRoute);
+router.get("/health", async (req, res) => res.send("ok"));
 router.use(passport.authenticate("jwt", { session: false }), usersRoute);
 module.exports = router;
